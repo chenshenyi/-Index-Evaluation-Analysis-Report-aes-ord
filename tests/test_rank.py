2dd8bb7b276562c7cdf6545a32a508a2eaa5176e
chenshenyi <123dtw@gmail.com>
2023-05-18 13:56:00 +0800

    delete python codes

--- tests/test_rank.py
+++
@@ -1,9 +0,0 @@
-from aes_tool.rank import ranking_list
-
-
-def test_ranking_list():
-    sortedlist = [1, 2, 2, 3, 4, 5]
-    assert ranking_list(sortedlist, 'dense') == (
-        1, 2, 2, 3, 4, 5), ranking_list(sortedlist, 'dense')
-    assert ranking_list(sortedlist, 'simple') == (
-        1, 2, 2, 4, 5, 6), ranking_list(sortedlist, 'simple')
